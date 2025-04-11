f3c697aa1f617784a2b6548f03832f10713ff8e9
Alesssandra F R <102836095+Alle84fr@users.noreply.github.com>
2025-04-10 21:00:15 -0300

    Create 02_fundamentos.kt

---
+++ 02_fundamentos.kt
@@ -0,0 +1,26 @@
+package um_introducao/* ir na pasta src onde está os arquivos
+programa inicia no método main
+funções = fun
+ */
+
+fun main (args: Array<String>){
+    print("Saluton Mondo")
+}
+
+/* depois apertar play - parte de cima, seta verde
+main = nome da função
+iniciar o parâmetros ()
+args = argumentos
+array = estrutura de dados que armazena elemntos DO MESMO TIPO, eficiêntes para operações numéricas
+de grande escala
+usase com:
+apenas números
+prcessamento numérico interno ( machine learnig, análise de dados)
+consome menos espaços
+
+obs
+lista[] | list[] = mutável, ordenada, índice inica no 0
+tuplas() | tuple() = imutáveis, ordenadas
+dicionário {chave:valor} | dict{} = mutável, não ordenado (chave e valor)
+conjunto {} | set{} = mutável, não ordenado, elemento único (não permite duplicados)
+ */
